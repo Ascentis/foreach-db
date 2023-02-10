d94550f8bf39e4cbe856a2980a035087aa51e73b
Jose Sebastian Battig <jsbattig@gmail.com>
2023-02-10 15:45:58 -0300

    Added code to expand the local copies of the deps SqlServer and PSWriteColor into the corresponding folder ${env:ProgramFiles}\WindowsPowerShell\Modules\ If the dep already exists the operation is skipped

--- Local-Install.ps1
+++ Local-Install.ps1
@@ -1,10 +1,24 @@
-$ErrorActionPreference = 'SilentlyContinue'
 $pathExists = Test-Path -Path ${env:ProgramFiles}\WindowsPowerShell\Modules\ForEach-DB
 if (!$pathExists) {
     New-Item ${env:ProgramFiles}\WindowsPowerShell\Modules\ForEach-DB -itemtype directory > $null    
 }
 Copy-Item .\ForEach-DB.psm1 ${env:ProgramFiles}\WindowsPowerShell\Modules\ForEach-DB
 Copy-Item .\ForEach-DB.psd1 ${env:ProgramFiles}\WindowsPowerShell\Modules\ForEach-DB
+
+$pathExists = Test-Path -Path ${env:ProgramFiles}\WindowsPowerShell\Modules\PSWriteColor
+if ($pathExists) {
+	Write-Output "PSWriteColor PowerShell package already installed"
+} else {
+	Expand-Archive .\deps\PSWriteColor.zip ${env:ProgramFiles}\WindowsPowerShell\Modules\ -Force
+}
+
+$pathExists = Test-Path -Path ${env:ProgramFiles}\WindowsPowerShell\Modules\SqlServer
+if ($pathExists) {
+	Write-Output "SqlServer PowerShell package already installed"
+} else {
+	Expand-Archive .\deps\SqlServer.zip ${env:ProgramFiles}\WindowsPowerShell\Modules\ -Force
+}
+
 try {
     remove-module -Name foreach-db
 } catch {
@@ -12,4 +26,7 @@ try {
         throw
     }
 }
+
+import-module PSWriteColor -DisableNameChecking
+import-module SqlServer -DisableNameChecking
 import-module foreach-db -DisableNameChecking
